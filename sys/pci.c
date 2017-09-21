b2e8dad8fb1b04709e39c4f52cf437503777d3ce
Prashanth <prashanths1992@gmail.com>
2017-09-21 14:46:12 -0400

    and

--- sys/pci.c
+++ sys/pci.c
@@ -5,8 +5,8 @@ void init_pci(){
     
     kprintf("%d",sizeof(struct pci_read));
     
-    struct pci_read p;
-    p.enableBit = 1;
+    struct pci_read* p;
+    p->enableBit = 1;
     
     kprintf("\n%d\n",p);
     
