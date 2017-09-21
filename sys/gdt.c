c2bfa420c1b4626d9a03e01126ce74bd3eeb3704
Prashanth <prashanths1992@gmail.com>
2017-09-21 14:37:04 -0400

    and

--- sys/gdt.c
+++ sys/gdt.c
@@ -79,6 +79,8 @@ void init_gdt() {
   sd->sd_hilimit = 0;
   sd->sd_gran = 0;
   sd->sd_hibase = ((uint64_t)&tss) >> 24;
+    
+    kprintf("Size of segment descriptor  %d", sizeof(sys_segment_descriptor));
 
   _x86_64_asm_lgdt(&gdtr, 8, 16);
   _x86_64_asm_ltr(0x28);
