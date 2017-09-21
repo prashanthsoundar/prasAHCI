69da9375767c559eafbc117d271dfdd54fb19363
Prashanth <prashanths1992@gmail.com>
2017-09-21 19:23:01 -0400

    sad

--- sys/pci.c
+++ sys/pci.c
@@ -30,7 +30,7 @@ void init_pci(){
         {
             struct pci_read pciRead={0,0x0C,0,j,i,0,1};
             outb_32((uint32_t)calConfigAddressSpace(pciRead),0xCF8);
-            kprintf("%d",inb_32(0xCFC));
+            kprintf("\n%d\n",inb_32(0xCFC));
             
         }
         
