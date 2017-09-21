15efe09c448b4f51c532169b60fb507e8f9bb3ef
Prashanth <prashanths1992@gmail.com>
2017-09-21 14:48:40 -0400

    ASD

--- sys/pci.c
+++ sys/pci.c
@@ -5,9 +5,9 @@ void init_pci(){
     
     kprintf("%d",sizeof(struct pci_read));
     struct pci_read a={0,0,0,0,0,0,1};
-    struct pci_read *p =a ;
-    p->enableBit = 1;
+    struct pci_read *p =&a ;
+   // p->enableBit = 1;
     
-    kprintf("\n%d\n",p);
+    kprintf("\n%d\n",*p);
     
 }
