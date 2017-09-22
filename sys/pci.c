0dc6161f8552114296be209068ee189f122fde0b
Prashanth <prashanths1992@gmail.com>
2017-09-22 00:37:18 -0400

    and

--- sys/pci.c
+++ sys/pci.c
@@ -31,41 +31,6 @@ int ifMultiFunction(uint16_t bus,uint16_t device)
     return readPIC(bus,device,0,0x0E) & (1<<7);
 }
 
-void decToHexa(int n)
-{
-    // char array to store hexadecimal number
-    char hexaDeciNum[100];
-    
-    // counter for hexadecimal number array
-    int i = 0;
-    while(n!=0)
-    {
-        // temporary variable to store remainder
-        int temp  = 0;
-        
-        // storing remainder in temp variable.
-        temp = n % 16;
-        
-        // check if temp < 10
-        if(temp < 10)
-        {
-            hexaDeciNum[i] = temp + 48;
-            i++;
-        }
-        else
-        {
-            hexaDeciNum[i] = temp + 55;
-            i++;
-        }
-        
-        n = n/16;
-    }
-    
-    // printing hexadecimal number array in reverse order
-    for(int j=i-1; j>=0; j--)
-        kprintf("%c",hexaDeciNum[j]);
-}
-
 void writePIC(uint16_t bus,uint16_t device,uint16_t function,uint32_t offset,uint32_t value)
 {
     uint32_t command = (uint32_t)((0x01<<31)|(bus&0xFF)<<16|(device&0x1F)<<11|(function&0x07)<<8|(offset&0xFC));
@@ -88,17 +53,6 @@ void printALLDrivers()
                 kprintf("Bus %d Device %d Function %d \n",bus,device,function);
                 kprintf("%x \n",(readPIC(bus,device,function,0x00)));
                 kprintf("%x \n",(readPIC(bus,device,function,0x08)));
-                
-//                decToHexa((readPIC(bus,device,function,0x00)&0xFF00)>>8);
-//                decToHexa((readPIC(bus,device,function,0x00)&0xFF));
-//                kprintf("\t");
-//                decToHexa((readPIC(bus,device,function,0x02)&0xFF00)>>8);
-//                decToHexa((readPIC(bus,device,function,0x02)&0xFF));
-//                kprintf("\t");
-//                decToHexa((readPIC(bus,device,function,0x08)&0xFF00)>>8);
-//                kprintf("\t");
-//                decToHexa((readPIC(bus,device,function,0x09)&0xFF)>>8);
-//                kprintf("\n");
                 kprintf("________________________\n");
                 }
             }
