c979dafb456922067f80ecf72f29458a7072f8ae
Prashanth <prashanths1992@gmail.com>
2017-09-21 23:37:57 -0400

    and

--- sys/pci.c
+++ sys/pci.c
@@ -85,7 +85,7 @@ void printALLDrivers()
                 uint32_t vendorID = readPIC(bus,device,function,0x00);
                 
                 if(vendorID==0xFFFF||vendorID==0x0000) break;
-                decToHexa(readPIC(bus,device,function,0x00))
+                decToHexa(readPIC(bus,device,function,0x00));
                    // decToHexa((readPIC(bus,device,function,0x00)&&0xFF)>>8);
                    // decToHexa(readPIC(bus,device,function,0x00)&&0xFF);
 //                    decToHexa((readPIC(bus,device,function,0x02)&&0xFFFF)>>8);
